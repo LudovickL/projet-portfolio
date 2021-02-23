42192712f4c61c5a8096de7c9a08722d3a5c467d
LudovickL <Luberisselud@gmail.com>
2021-02-23 11:06:01 -0500

    Logo + header de page d'acceuil

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,10 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="./Logo_portfolio.png" className="App-logo" alt="logo" />
-        <p>
-          Le portfolio officiel de Ludovick lubérisse est en construction. Pour voir le portfolio de sauvetage,
-        </p>
-        <a
-          className="App-link"
-          href="https://ludovick-portfolio.webflow.io/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          veuillez vous rendre ici
-        </a>
-      </header>
+      <div className="App-section">
+          <img src="./Logo_portfolio.png" className="App-logo" alt="logo" />
+          <h1 className="App-title">Acceuil</h1> 
+      </div>
     </div>
   );
 }
