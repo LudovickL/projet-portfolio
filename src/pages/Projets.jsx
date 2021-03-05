ae0c590bb2020d4cf9753903a0be96b527383715
LudovickL <Luberisselud@gmail.com>
2021-03-05 12:15:49 -0500

    modifications mineur à la page projet

--- src/pages/Projets.jsx
+++ src/pages/Projets.jsx
@@ -1,12 +1,11 @@
 import './Pages.scss';
-import './plusieursProjets.scss';
 import UnProjet from '../components/UnProjet';
 
 export default function Projets() {
     return(
         <div className="Page">
             <h1 className="Titre">Projets - Page en construction</h1>
-            <div className="projets">
+            <div className="listeProjets">
                 <UnProjet />
                 <UnProjet />
             </div> 
