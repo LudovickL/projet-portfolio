af86f3d21081c9671833fcca233a8ef0b9cb6290
LudovickL <Luberisselud@gmail.com>
2021-03-05 12:04:43 -0500

    Projets

--- src/pages/Projets.jsx
+++ src/pages/Projets.jsx
@@ -1,11 +1,16 @@
 import './Pages.scss';
+import './plusieursProjets.scss';
 import UnProjet from '../components/UnProjet';
 
 export default function Projets() {
     return(
         <div className="Page">
-            <h1 className="Titre">Projets - Page en construction</h1> 
-            <UnProjet></UnProjet>
+            <h1 className="Titre">Projets - Page en construction</h1>
+            <div className="projets">
+                <UnProjet />
+                <UnProjet />
+            </div> 
+            
         </div>
     )
 }
