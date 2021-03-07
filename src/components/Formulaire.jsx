a6f54c9f9ad8734583c742818d6fa7b29533531d
LudovickL <Luberisselud@gmail.com>
2021-03-06 22:40:03 -0500

    finissions du formulaire (frontend)

--- src/components/Formulaire.jsx
+++ src/components/Formulaire.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 import './Formulaire.scss';
 
+//Le code pour cette classe fut trouvé sur le blog mailtrap dans un article créer par Aleksandr Varnin:
+// https://blog.mailtrap.io/react-contact-form/#How_do_you_create_a_contact_form_with_React
+
+// Il y a aussi l'article de Victoria Lo sur le sujet:
+// https://medium.com/verclaire-nine/how-to-build-a-contact-form-with-react-and-nodemailer-3ca105eff797
 class Formulaire extends React.Component {
     constructor(props) {
       super(props);
@@ -34,20 +40,34 @@ class Formulaire extends React.Component {
         </div>
       );
     }
-
+    // Le nom de la personne qui envoie le message va être enregistrer
     onNameChange(event) {
         this.setState({name: event.target.value})
       }
-    
+    // L'adresse courriel de la personne qui envoie le message va être enregistrer
       onEmailChange(event) {
         this.setState({email: event.target.value})
       }
-    
+    // Le nessage de la personne va être enregistrer
       onMessageChange(event) {
         this.setState({message: event.target.value})
       }
-    
+    // Le message sera soumit et envoyer à l'adresse email voulu
       handleSubmit(event) {
+        event.preventDefault();
+        
+        axios({
+          method:"POST",
+          url:"/send",
+          data: this.state
+        }).then((response)=>{
+          if(response.data.status === 'success') {
+            alert("votre message a été envoyé!");
+            this.resetForm()
+          } else if(response.data.status === 'fail') {
+            alert("Il y a eu une erreur avec votre message. Veuillez réessayer.")
+          }
+        })
       }
     }
     
