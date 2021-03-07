c53910be230faa121606060be77af98c511306c4
LudovickL <Luberisselud@gmail.com>
2021-03-07 14:42:30 -0500

    Finission du formulaire de contacte

--- src/components/Formulaire.jsx
+++ src/components/Formulaire.jsx
@@ -1,80 +1,44 @@
+import emailjs from 'emailjs-com';
 import React from 'react';
-import axios from 'axios';
 import './Formulaire.scss';
-//import sendMail from './data/sendMail.js';
 
-//Le code pour cette classe fut trouvé sur le blog mailtrap dans un article créer par Aleksandr Varnin:
-// https://blog.mailtrap.io/react-contact-form/#How_do_you_create_a_contact_form_with_React
+function Formulaire() {
 
-// Il y a aussi l'article de Victoria Lo sur le sujet:
-// https://medium.com/verclaire-nine/how-to-build-a-contact-form-with-react-and-nodemailer-3ca105eff797
-class Formulaire extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        name: '',
-        email: '',
-        message: ''
-      }
-    }
-  
-    render() {
-      return(
-        <div className="Formulaire">
-          <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange.bind(this)} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Email address</label>
-              <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Message</label>
-              <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
-            </div>
-            <div className="form-group">
-                <input type="submit" className="button" value="Envoyer"></input>
-            </div>
-          </form>
+  let msgBouton = "Envoyer";
+
+  function sendMail(e) {
+    e.preventDefault();
+    emailjs.sendForm('service_slsnfcn', 'template_ipesxvr', e.target, 'user_TpRk119LuedlrSVLQDsdx')
+      .then((result) => {
+          console.log(result.text);
+          alert("Le message à été envoyé!");
+          document.getElementsByClassName("button").innerHTML = "message envoyé"
+      }, (error) => {
+          console.log(error.text);
+          alert("Il y a malheureusement un problème. Veuillez réasseyer plus tard");
+      });
+      e.target.reset();
+  }
+  return(
+    <div className="Formulaire">
+      <form id="contact-form" onSubmit={sendMail}>
+        <div className="form-group">
+          <label htmlFor="name">Nom</label>
+          <input type="text" className="form-control" name="name"/>
         </div>
-      );
-    }
-    // Le nom de la personne qui envoie le message va être enregistrer
-    onNameChange(event) {
-        this.setState({name: event.target.value})
-      }
-    // L'adresse courriel de la personne qui envoie le message va être enregistrer
-      onEmailChange(event) {
-        this.setState({email: event.target.value})
-      }
-    // Le nessage de la personne va être enregistrer
-      onMessageChange(event) {
-        this.setState({message: event.target.value})
-      }
-    // Le message sera soumit et envoyer à l'adresse email voulu
-      handleSubmit(event) {
-        event.preventDefault();
-        
-        axios({
-          method:"POST",
-          url:"http://localhost:3002/send",
-          data: this.state
-        }).then((response)=>{
-          if(response.data.status === 'success') {
-            alert("votre message a été envoyé!");
-            this.resetForm()
-          } else if(response.data.status === 'fail') {
-            alert("Il y a eu une erreur avec votre message. Veuillez réessayer.")
-          }
-        })
-      }
-      resetForm(){
-        this.setState({name: '', email: '', message: ''})
-      }
-    }
-
-    
-    
-    export default Formulaire;
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Adresse courriel</label>
+          <input type="email" className="form-control" aria-describedby="emailHelp" name="email"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea className="form-control" rows="5" name="message"/>
+        </div>
+        <div className="form-group">
+            <input type="submit" className="button" value={msgBouton}></input>
+        </div>
+      </form>
+    </div>
+  );
+}
+export default Formulaire;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ import './Pages.scss';
 export default function Contact() {
     return(
         <div className="Page">
-            <h1 className="Titre">Contact - Page en construction</h1> 
+            <h1 className="Titre">Contact</h1> 
             <p className="texte-contact">Pour toute question ou idées que vous voulez réaliser avec moi, envoyer un message ici!</p>
             <div className="Formulaire">
                 <Formulaire></Formulaire>
