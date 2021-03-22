d9ea82f5b0c67e673819a39e66015b1249b096e4
LudovickL <Luberisselud@gmail.com>
2021-03-22 18:48:25 -0400

    Amélioration du responsive (v2)

--- src/components/UnProjet.jsx
+++ src/components/UnProjet.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import './UnProjet.scss'
 
 const UnProjet = ({projets}) => {
-    console.log(projets)
-    const {texteCourt, image, lienDuProjet} = projets.fields;
+    //console.log(projets)
+    const {texteCourt, image} = projets.fields;
     return(
         <div className="Projet-conteneur">
             <div className="projet-logo">
                 <h1 className="projet-title">{texteCourt}</h1>
-                <img src={image.fields.file.url} alt={texteCourt} className="projet-image" alt="logo" /> 
+                <img src={image.fields.file.url} alt={texteCourt} className="projet-image"/> 
             </div>
         </div>
     )
